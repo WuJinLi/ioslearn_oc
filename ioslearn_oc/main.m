a38d2cc50ceddf6f98d237f99e207f0b0823fe8b
wujinli <wujinli>
2020-06-18 15:07:45 +0800

    新建项目，创建本地与远程库之间的关联

---
+++ ioslearn_oc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ioslearn_oc
+//
+//  Created by WuJInLi on 2020/6/18.
+//  Copyright © 2020 WuJInLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
